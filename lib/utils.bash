1e4b23016d0ca797a6be68cf2caabf321c1fc082
Samuele Chiocca <samuele@sighup.io>
2022-06-17 17:19:19 +0200

    Fixing install step

--- lib/utils.bash
+++ lib/utils.bash
@@ -67,7 +67,7 @@ download_release() {
 install_version() {
   local install_type="$1"
   local version="$2"
-  local install_path="$3/kuma-${version}/bin"
+  local install_path="$3"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
@@ -75,7 +75,7 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/"kuma-${version}/bin/"* "$install_path"
 
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
