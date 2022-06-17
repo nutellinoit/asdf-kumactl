fb29592b5bb79adfad53fcfcdae670fe893dec08
Samuele Chiocca <samuele@sighup.io>
2022-06-17 17:08:22 +0200

    Add sleep for testing

--- lib/utils.bash
+++ lib/utils.bash
@@ -78,7 +78,7 @@ install_version() {
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
-
+    sleep 3600
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
     rm -rf "$install_path"
